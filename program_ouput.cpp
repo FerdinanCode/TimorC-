b96a5e6475e141b4a59ad31c407f02d89defc890
FerdinanCode <121213176+FerdinanCode@users.noreply.github.com>
2023-01-03 21:28:58 +0700

    Add files via upload

---
+++ program_ouput.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"========= Program Output==========" <<endl;
+	cout<<"Program ini adalah program untuk\n";
+	cout<<"Menampilkan output ke layar komputer\n";
+	cout<<"Output sangat penting untuk menampilkan\n";
+	cout<<"informasi kepada user.\n";
+	cout<<"==================================" <<endl;
+	
+	cout<<endl;
+	cout<<"Program ini dibuat oleh Ferdinan" <<endl;
+	
+	return 0;
+}
---
+++ program_output_printf.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	printf("Hello, ini adalah teks ouput\n");
+	printf("Nama saya %s\n", "Ferdinan");
+	printf("Usia saya %n", 20);
+	return 0;
+}
